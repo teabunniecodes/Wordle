9a72cacd9e2803f0004077abae9d8cd6c0a9ead2
Bunnie <bunniecodes@gmail.com>
2022-05-14 14:33:19 -0700

    commit

--- wordleV3.py
+++ wordleV3.py
@@ -22,45 +22,33 @@ class Wordle:
     def color_alphabet(self):
         for x, letter in enumerate(self.guess):
             if self.guess[x] == self.chosen_word[x]:
-                self.alphabet = list(map(lambda x: x.replace(letter.upper(), self.set_color(letter, Color.GREEN, Color.END)), self.alphabet))
+                self.alphabet = list(map(lambda x: x.replace(letter.upper(), self.set_color(letter.upper(), Color.GREEN)), self.alphabet))
         
             elif letter in self.chosen_word and letter.upper() in self.alphabet:
-                self.alphabet = list(map(lambda x: x.replace(letter.upper(), self.set_color(letter.upper(), Color.YELLOW, Color.END)), self.alphabet))   
+                self.alphabet = list(map(lambda x: x.replace(letter.upper(), self.set_color(letter.upper(), Color.YELLOW)), self.alphabet))
+            
+            elif letter not in self.chosen_word and letter.upper() in self.alphabet:
+                self.alphabet = list(map(lambda x: x.replace(letter.upper(), self.set_color(letter.upper(), Color.RED)), self.alphabet))
 
-            elif letter not in self.chosen_word:
-                self.alphabet = list(map(lambda x: x.replace(letter.upper(), self.set_color(letter.upper(), Color.RED, Color.END)), self.alphabet))
+            else:
+                pass
 
-    def set_color(self, text, color, end):
-        return f"{color}{text.upper()}{end}"
+    def set_color(self, text, color):
+        return f"{color}{text.upper()}{Color.END}"
     
     def get_word(self):
         # gets a random word from a text document
         with open("words.txt") as read:
-            words = list(map(str, read))
-            self.chosen_word = random.choice(words).strip().upper()
+            self.words = list(map(str, read))
+            self.chosen_word = random.choice(self.words).strip().upper()
     
     def get_guess(self):
         self.guess = input(f"Turn {self.turns}: ").upper()
 
     def check_dictionary(self):
         with open("dictionary.txt") as dictionary:
-            dictionary = dictionary.read()
-            dictionary = dictionary.strip().split("\n")
-            if self.guess.lower() in dictionary:
-                self.turns -= 1
-                self.is_win()
-                # TODO compare_guess method needs to go here
-                self.color_guess()
-                self.display_alphabet()
-            elif self.guess.isalpha():
-                if len(self.guess) < 5:
-                    print("The word is to short!")
-                elif len(self.guess) > 5:
-                    print("The word is to short!")
-                elif len(self.guess) == 5:
-                    print("Not a word in the dictionary")
-            else:
-                print("Not a valid word -_-")
+            self.dictionary = dictionary.read()
+            self.dictionary = self.dictionary.strip().split("\n")
 
     def color_guess(self):
         guess_letters = list(self.guess)
@@ -68,38 +56,65 @@ class Wordle:
         for x, letter in enumerate(self.guess):
             if guess_letters[x] == self.chosen_word[x]:
                 chosen_letters[x] = "*"
-                guess_letters[x] = self.set_color(letter, Color.GREEN, Color.END)
+                guess_letters[x] = self.set_color(letter, Color.GREEN)
         for x, letter in enumerate(self.guess):
             if letter in chosen_letters:
                 if self.guess[x] != self.chosen_word[x]:
                     # need to replace at the index of the letter
                     chosen_letters[chosen_letters.index(letter)] = "*"
-                    guess_letters[x] = self.set_color(letter, Color.YELLOW, Color.END)
+                    guess_letters[x] = self.set_color(letter, Color.YELLOW)
         print("".join(guess_letters))
 
     def check_guess(self):
-        while True:
-            if self.turns > 0:
-                self.get_guess()
-                self.check_dictionary()
-                self.is_lose()
+        if self.guess.lower() in self.dictionary or self.words:
+                self.turns -= 1
+                self.is_win()
+                # TODO compare_guess method needs to go here
+                self.color_guess()
+                self.display_alphabet()
+        elif self.guess.isalpha():
+            if len(self.guess) < 5:
+                print("The word is to short!")
+            elif len(self.guess) > 5:
+                print("The word is to long!")
+            elif len(self.guess) == 5:
+                print("Not a word in the dictionary")
+        else:
+            print("Not a valid word -_-")
 
     def is_win(self):
         if self.guess == self.chosen_word:
             self.color_guess()
             print("You Won!")
-            exit()
+            self.play_again()
 
     def is_lose(self):
         if self.turns == 0:
             print("You Lost :(")
             print(f"The word was {self.chosen_word.upper()}")
-            exit()
+            self.play_again()
+
+    def play_again(self):
+        answer_list = ["Y", "N"]
+        while True:
+            answer = input("Would you like to play again (Y/N)? ").upper()
+            if answer == answer_list[0]:
+                self.gameplay()
+            elif answer == answer_list[1]:
+                print("Thank you for playing!")
+                exit()
+            else:
+                print("Please enter a valid option")
+
 
     def gameplay(self):
         self.get_word()
-        self.check_guess()
-
+        while True:
+            if self.turns > 0:
+                self.get_guess()
+                self.check_dictionary()
+                self.check_guess()
+                self.is_lose()
 
 wordle = Wordle()
 wordle.gameplay()
